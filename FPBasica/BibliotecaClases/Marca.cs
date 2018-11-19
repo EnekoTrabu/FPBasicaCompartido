53116861e0b99eb461a720639b01cb43ab2e9625
Xabier San Martín Oneka <xabottol@hotmail.com>
2018-11-19 14:09:15 +0100

    V1: Atributos(getter y setter) contructor vacio y con todos los parametros y el IEquatable

--- FPBasica/BibliotecaClases/Marca.cs
+++ FPBasica/BibliotecaClases/Marca.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace BibliotecaClases
 {
-    class Marca
+    class Marca : IEquatable<Marca>
     {
+        public int idMarca { get; set; }
+        public string nombre { get; set; }
+
+        public Marca()
+        {
+        }
+
+        public Marca(int idMarca, string nombre)
+        {
+            this.idMarca = idMarca;
+            this.nombre = nombre;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Marca);
+        }
+
+        public bool Equals(Marca other)
+        {
+            return other != null &&
+                   idMarca == other.idMarca;
+        }
+
+        public override int GetHashCode()
+        {
+            return 1306757816 + idMarca.GetHashCode();
+        }
     }
 }
