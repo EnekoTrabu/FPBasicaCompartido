4bff7c4d7333d6f80da8013861ac4525a887e6e7
Xabier San Martín Oneka <xabottol@hotmail.com>
2018-11-19 13:54:14 +0100

    v1, Estante probado (me da fallos no me crea cosas)

--- FPBasica/BibliotecaClases/Estante.cs
+++ FPBasica/BibliotecaClases/Estante.cs
@@ -8,5 +8,24 @@ namespace BibliotecaClases
 {
     class Estante
     {
+        public char idEstanteria { get; set; }
+        public int altura {get; set;}
+        public string estante {get; set;}
+
+        public Estante()
+        {
+        }
+
+         public Estante(char idEstanteria, int altura, string estante)
+        {
+            this.idEstanteria = idEstanteria;
+            this.altura= altura;
+            this.estante = estante;
+        }
+
+
+
+
+
     }
 }
