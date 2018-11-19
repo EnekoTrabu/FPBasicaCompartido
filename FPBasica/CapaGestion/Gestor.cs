72216bc1122d8170113ee9939a684f9e66f1cf21
Permintong <36698347+Permintong@users.noreply.github.com>
2018-11-19 13:22:16 +0100

    v1 gestor a ver si ahora funciona

--- FPBasica/CapaGestion/Gestor.cs
+++ FPBasica/CapaGestion/Gestor.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using BibliotecaClases;
 
 namespace CapaGestion
 {
     public class Gestor
     {
+
+        private MySqlConnection conexion;
+        private string servidor;
+        private string BD;
+        private string Usuario;
+        private string Contrasena;
+        private string IniciarConexion()
+        {
+            servidor = "localhost";
+            BD = "agenda";
+            Usuario = "root";
+            Contrasena = "";
+            string stringDeConexion;
+            stringDeConexion = "SERVER=" + servidor + ";" + "DATABASE=" +
+            BD + ";" + "UID=" + Usuario + ";" + "PASSWORD=" + Contrasena + ";";
+            try
+            {
+                conexion = new MySqlConnection(stringDeConexion);
+                conexion.Open();
+                return "Conexión exitosa";
+            }
+            catch (Exception elCasque)
+            {
+
+                return elCasque.Message ;
+            }
+        }
+
     }
 }
