2f258590c3dd260c579eeccca86fc9b30abdcdf6
Aritz <Aritz@DESKTOP-14ALJML>
2018-11-19 13:32:20 +0100

    clase familia

--- FPBasica/BibliotecaClases/Familia.cs
+++ FPBasica/BibliotecaClases/Familia.cs
@@ -6,7 +6,48 @@ using System.Threading.Tasks;
 
 namespace BibliotecaClases
 {
-    class Familia
+    class Familia : IEquatable<Familia>
     {
+        public string Idfamilia { get; set; }
+        public string IdString { get; set; }
+        public string Descripcion { get; set; }
+
+        public Familia(string idfamilia, string idString, string descripcion)
+        {
+            Idfamilia = idfamilia;
+            IdString = idString;
+            Descripcion = descripcion;
+        }
+
+        public Familia(string idfamilia, string idString)
+        {
+            Idfamilia = idfamilia;
+            IdString = idString;
+        }
+
+        public Familia()
+        {
+        }
+
+        public Familia(string idfamilia)
+        {
+            Idfamilia = idfamilia;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Familia);
+        }
+
+        public bool Equals(Familia other)
+        {
+            return other != null &&
+                   Idfamilia == other.Idfamilia;
+        }
+
+        public override int GetHashCode()
+        {
+            return -514772085 + EqualityComparer<string>.Default.GetHashCode(Idfamilia);
+        }
     }
 }
