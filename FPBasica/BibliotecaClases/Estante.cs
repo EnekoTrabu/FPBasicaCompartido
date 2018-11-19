7eddcb0fd5168646d5829785628ab3afbf100726
Xabier San Martín Oneka <xabottol@hotmail.com>
2018-11-19 13:59:52 +0100

    v2: Estante con sus atributos(getter y setter) constructores y el IEquatable

--- FPBasica/BibliotecaClases/Estante.cs
+++ FPBasica/BibliotecaClases/Estante.cs
@@ -6,26 +6,43 @@ using System.Threading.Tasks;
 
 namespace BibliotecaClases
 {
-    class Estante
+   class Estante : IEquatable<Estante>
     {
         public char idEstanteria { get; set; }
-        public int altura {get; set;}
-        public string estante {get; set;}
+        public int altura { get; set; }
+        public string estante { get; set; }
 
         public Estante()
         {
         }
 
-         public Estante(char idEstanteria, int altura, string estante)
+        public Estante(char idEstanteria, int altura, string estante)
         {
             this.idEstanteria = idEstanteria;
-            this.altura= altura;
+            this.altura = altura;
             this.estante = estante;
         }
 
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Estante);
+        }
 
+        public bool Equals(Estante other)
+        {
+            return other != null &&
+                   idEstanteria == other.idEstanteria &&
+                   altura == other.altura &&
+                   estante == other.estante;
+        }
 
-
-
+        public override int GetHashCode()
+        {
+            var hashCode = 2107790685;
+            hashCode = hashCode * -1521134295 + idEstanteria.GetHashCode();
+            hashCode = hashCode * -1521134295 + altura.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(estante);
+            return hashCode;
+        }
     }
 }
