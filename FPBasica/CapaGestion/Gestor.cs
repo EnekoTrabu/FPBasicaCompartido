272271dc0e488837a70e5a2544e28325bb391071
Permintong <36698347+Permintong@users.noreply.github.com>
2018-11-26 13:21:59 +0100

    v1: cambios en el gestor

--- FPBasica/CapaGestion/Gestor.cs
+++ FPBasica/CapaGestion/Gestor.cs
@@ -19,7 +19,7 @@ namespace CapaGestion
         private string IniciarConexion()
         {
             servidor = "localhost";
-            BD = "agenda";
+            BD = "almacenbd";
             Usuario = "root";
             Contrasena = "";
             string stringDeConexion;
@@ -39,22 +39,62 @@ namespace CapaGestion
         }
         private void InsertarProducto(Producto producto)
         {
-            string InsertContacto = "INSERT INTO producto (codigo, codbarras, descripcion, idmarca, precio, stock, pesobruto, pesoneto, estanteria, altura, estante) VALUES(@codigo, @codbarras, @descripcion, @idmarca, @precio, @stock, @pesobruto, @pesoneto, @estanteria, @altura, @estante);";
-            MySqlCommand cmdIContacto = new MySqlCommand(InsertContacto, conexion);
-            cmdIContacto.Parameters.AddWithValue("@codigo", nombre);
-            cmdIContacto.Parameters.AddWithValue("@codbarras", email);
-            cmdIContacto.Parameters.AddWithValue("@descripcion", nombre);
-            cmdIContacto.Parameters.AddWithValue("@idmarca", email);
-            cmdIContacto.Parameters.AddWithValue("@precio", nombre);
-            cmdIContacto.Parameters.AddWithValue("@stock", email);
-            cmdIContacto.Parameters.AddWithValue("@pesobruto", nombre);
-            cmdIContacto.Parameters.AddWithValue("@pesoneto", email);
-            cmdIContacto.Parameters.AddWithValue("@estanteria", nombre);
-            cmdIContacto.Parameters.AddWithValue("@altura", email);
-            cmdIContacto.Parameters.AddWithValue("@estante", nombre);
-      
-            cmdIContacto.ExecuteNonQuery();
+           try 
+	       {	        
+		       string InsertContacto = "INSERT INTO producto (codigo, codbarras, descripcion, idmarca, precio, stock, pesobruto, pesoneto, estanteria, altura, estante) VALUES(@codigo, @codbarras, @descripcion, @idmarca, @precio, @stock, @pesobruto, @pesoneto, @estanteria, @altura, @estante);";
+               MySqlCommand cmdIContacto = new MySqlCommand(InsertContacto, conexion);
+               cmdIContacto.Parameters.AddWithValue("@codigo", nombre);
+               cmdIContacto.Parameters.AddWithValue("@codbarras", email);
+               cmdIContacto.Parameters.AddWithValue("@descripcion", nombre);
+               cmdIContacto.Parameters.AddWithValue("@idmarca", email);
+               cmdIContacto.Parameters.AddWithValue("@precio", nombre);
+               cmdIContacto.Parameters.AddWithValue("@stock", email);
+               cmdIContacto.Parameters.AddWithValue("@pesobruto", nombre);
+               cmdIContacto.Parameters.AddWithValue("@pesoneto", email);
+               cmdIContacto.Parameters.AddWithValue("@estanteria", nombre);
+               cmdIContacto.Parameters.AddWithValue("@altura", email);
+               cmdIContacto.Parameters.AddWithValue("@estante", nombre);
+               cmdIContacto.ExecuteNonQuery();
+	       }
+	       catch (Exception elCasque)
+           {
+                return elCasque.Message ;
+           }
         }
+        private List<Producto> classicSelectProducto(string atributo, string valor){
+            string SelectDatos = "SELECT * FROM producto WHERE @atributo=@valor;";
+                MySqlCommand cmdS = new MySqlCommand(SelectDatos, conexion);
+                cmdS.Parameters.AddWithValue("@atributo", atributo);
+                cmdS.Parameters.AddWithValue("@valor", valor);
+                MySqlDataReader Lector = cmdS.ExecuteReader();
+                List<Producto> listado = new List<Producto>();
+                while (Lector.Read())
+                {
+                        listado.Add(new Producto());
+ 
+                }
+                Lector.Close();
+                conexion.Close();
+                
+                return listado;
+        }
+        private void classicSelectProducto(string trozoDescripcion){
+            string SelectDatos = "SELECT * FROM producto WHERE producto.descripcion LIKE @valor;";
+                MySqlCommand cmdS = new MySqlCommand(SelectDatos, conexion);
+                cmdS.Parameters.AddWithValue("@valor", ("%"+ trozoDescripcion+ "%"));
+                MySqlDataReader Lector = cmdS.ExecuteReader();
+                List<string> listado = new List<string>();
+        }
+        public string DarAltaProducto(Producto nuevoProducto){
 
+            IniciarConexion();
+            if(InsertarProducto(nuevoProducto)){
+                return "Producto añadido con éxito";
+
+            }else{
+                return "Hubo un problema al añadir el producto";
+            }
+            
+        }
     }
 }
